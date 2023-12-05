aed62a96089aed3ad5c4fe5f40cfa7458c85ce2f
felipeBarras <felipeu_santos@hotmail.com>
2023-12-05 12:02:30 -0300

    trying to figure out how to export form data

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-require('dotenv').config();
+require('dotenv').config(); // carregando variáveis de ambiente do .env file
 
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+
+console.log(process.env.CONNECTIONSTRING);
+
 mongoose
-  .connect(process.env.CONNECTIONSTRING)
-  .then(() => app.emit('emitido'))
-  .catch((e) => console.log(e));
+  .connect(process.env.CONNECTIONSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Conectado ao MongoDB');
+    app.emit('emitido');
+  })
+  .catch((error) => console.log(`Erro ao conectar no MongoDB: ${error}`));
 
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -14,12 +23,7 @@ const flash = require('connect-flash');
 const routes = require('./routes');
 const path = require('path');
 const helmet = require('helmet');
-const csrf = require('csurf');
-const {
-  middlewareGlobal,
-  checkCsrfError,
-  csrfMiddleware
-} = require('./src/middlewares/middleware');
+const { middlewareGlobal } = require('./src/middlewares/middleware');
 
 app.use(
   helmet({
@@ -42,47 +46,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
-const sessionOptions = session({
-  secret: 'a812yu3281u281u8j1a8j18qwj1ajqu8qu1()',
-  store: MongoStore.create({
-    mongoUrl: process.env.CONNECTIONSTRING,
-    dbName: 'cursojs01'
-  }),
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-    httpOnly: true
-  }
-});
-
-app.use(sessionOptions);
 app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
-app.use(csrf());
 // Nossos próprios middlewares
 app.use(middlewareGlobal);
-app.use(checkCsrfError);
-app.use(csrfMiddleware);
-
 app.use(routes);
-
-app.get('/teste/:idUsers?/:param?', (req, res) => {
-  console.log(req.params);
-  console.log(req.query);
-  res.send(req.query.nome);
-});
-
-app.post('/', (req, res) => {
-  console.log(req.body);
-  res.send(`O que você enviou foi: ${req.body.qualquercoisa}`);
-});
-
-app.get('/contato', (req, res) => {
-  res.send('Obrigado por contatar!');
-});
 
 app.on('emitido', () => {
   app.listen(3000, () => {
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -2,16 +2,3 @@ exports.middlewareGlobal = (req, res, next) => {
   res.locals.localVariable = 'Este é o valor da variável local';
   next();
 };
-
-exports.checkCsrfError = (err, req, res, next) => {
-  if (err) {
-    return res.render('404');
-  }
-
-  next();
-};
-
-exports.csrfMiddleware = (req, res, next) => {
-  res.locals.csrfToken = req.csrfToken();
-  next();
-};
